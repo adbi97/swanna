70c45ec3a45d0f874f7cc1591f87da5b2b2c8601
adbi97 <aadibahsan97@gmail.com>
2024-09-05 16:39:34 +1000

    Updated variable name

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -18,7 +18,7 @@ variable "aws_secret_access_key" {
 
 variable "terraform_state_bucket" {
   description = "Name of bucket where Terraform state files are stored"
-  default     = "state-bucket"
+  default     = "state-bucket-swanna"
 }
 
 variable "dynamodb_table_name" {
